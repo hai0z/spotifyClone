9a421a7d0beb66c989e1bac31cab82bfbc3c5ffe
hai0z <hainguyen56211@gmail.com>
2023-01-08 13:00:18 +0700

    add search debounce

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+const useDebounce = <T>(value: T, delay: number) => {
+    const [debounceValue, setDebounceValue] = useState(value);
+
+    useEffect(() => {
+        const handle = setTimeout(() => setDebounceValue(value), delay);
+
+        return () => clearTimeout(handle);
+    }, [value, delay]);
+
+    return debounceValue;
+};
+
+export default useDebounce;
